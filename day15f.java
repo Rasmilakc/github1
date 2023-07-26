e0fcd1ba03919691ce1619ac72ceaa6d53c00a33
Rasmilakc <139522774+Rasmilakc@users.noreply.github.com>
2023-07-25 20:02:00 -0500

    added

--- day15f.java
+++ day15f.java
@@ -1,28 +1,30 @@
-package Test3;
+package Test3; 
 
-
-public class day15e {
-
+public class day15f {
+	
 	public static void main(String[] args) {
-		int Order=Sizeab.SMALL.getsize();
-		System.out.println(Order);
-
+		int pant=Sizees.SMALL.getsize();
+		System.out.println(pant);
+		
 	}
-
-}enum Sizeab{
+	
+}
+enum Sizees{
 	SMALL,MEDIUM,LARGE,EXTRALARGE;
 	public int getsize() {
 		switch(this) {
 		case SMALL:
-			return 3;
+			return 1;
 		case MEDIUM:
-			return 3;
+			return 2;
 		case LARGE:
 			return 3;
 		case EXTRALARGE:
-			return 3;
+			return 4;
+			
 			default:
 				return 0;
 		}
 	}
-}
+} 
+
