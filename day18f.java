27f21c3c5a10fd36c3c86e588b8a2fc4cc704d01
Rasmilakc <139522774+Rasmilakc@users.noreply.github.com>
2023-08-01 22:26:05 -0500

    Add files via upload

---
+++ day18f.java
@@ -0,0 +1,33 @@
+package Test3;
+
+//public class day18f {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		let names=["Rasmila","Khatri",22,33]
+//				let info={
+//				    "firstName":"Rasmila",
+//				    "lastName":"Khatri",
+//				    "age":22,
+//				    "rollNo":33,
+//				}
+//				//retrive--(dot notation and bracket notation)
+//				console.log(info.firstName)
+//				console.log(info['firstName'])
+//				// add (dot notation and bracket notation)
+//				info.city="kathmandu"
+//				info['language']="English"
+//				console.log(info)
+//				// update (dot notation and bracket notation)
+//				info.city="irving"
+//				info['language']="Nepali"
+//				console.log(info)
+//				//delete(dot notation and bracket notation)
+//				delete info.city
+//				delete info['language']
+//				console.log(info)
+//
+//	}
+//
+//}
+//
